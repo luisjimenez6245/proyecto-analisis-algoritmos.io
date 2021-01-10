4886780de8cb09de123ac21120d13d26f15fdd06
Luis Jiménez <luisjimenez6245@hotmail.com>
2021-01-10 13:54:08 -0600

    agrega estructura

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from 'components/Navbar'
+import Footer from 'components/Footer'
+import Home from 'components/Home'
+
+function App() {
+  return (
+   <>
+      <Navbar></Navbar>
+        <Home></Home>
+      <Footer></Footer>
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+export default function Home(){
+    return(
+        <section className="hero is-fullheight-with-navbar">
+            a
+        </section>
+    )
+}
